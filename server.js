d95b234b1b9950a4a33363dd5e1d5c5df2d7a8cf
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-13 15:14:48 +0100

    implement db.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const routes = require('./routes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 app.listen(PORT, (error) => {
     if (error) {
         console.log(error);
     } else {
-        console.log(`Server started and listening on port ${PORT} ... `);
+        console.log(`Server started and listening on port ${PORT} ...`);
     }
-});
+})
