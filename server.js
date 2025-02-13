81f0f4bb09b131d969b054ffa43b211fc3f40658
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-13 15:35:30 +0100

    bind initdb into server.js

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const routes = require('./routes');
+const init = require('./initdb');
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/init', init);
 app.use('/', routes);
 
 app.listen(PORT, (error) => {
