30ad1a90cc20cffd4c9bf49d225eb2c81d5ede89
Melisa Cihan <melisa-cihan@outlook.de>
2025-02-27 12:24:11 +0100

    fix main to server.js and add requirements

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const pg = require('pg');
+require('dotenv').config();
 
 const client = new pg.Client({
     user: process.env.PGUSER,
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const client = require('./db')
 
 //CRUD
 // GET all assets
